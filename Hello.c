ddaf6c7fe1e7440ea3a6512d480655344fcfb832
CrasherCourse <micha454@umn.edu>
2013-03-03 13:40:37 -0600

    Added Hello.c

---
+++ Hello.c
@@ -0,0 +1,16 @@
+// This is a generic Wello Horld code
+// Written in C
+
+// Include
+#include <stdio.h>
+
+// Defines
+
+/**********************************************
+ * Code
+ **********************************************/
+
+int main(int argc, char** argv)
+{
+    printf("Wello Horld!\n");
+}
